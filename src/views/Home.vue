47c7db9d325ab1f50723e73f1e16adb05e84a233
Grumpy Bear <grumpybear4257@gmail.com>
2021-01-06 14:21:14 -0700

    work on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,20 +11,28 @@
       <v-row>
         <v-col sm="3">
           <a href="https://play.google.com/store/apps/details?id=com.livetl.android">
-            <v-img src="@/assets/install-android.png" v-bind="attrs" v-on="on"/>
+            <v-img src="@/assets/store-images/install-android.png"/>
           </a>
         </v-col>
 
         <v-col sm="3">
           <a href="https://chrome.google.com/webstore/detail/livetl-live-translations/moicohcfhhbmmngneghfjfjpdobmmnlg">
-            <v-img src="@/assets/install-chrome.png"/>
+            <v-img src="@/assets/store-images/install-chrome.png"/>
           </a>
         </v-col>
 
         <v-col sm="3">
           <a href="https://addons.mozilla.org/en-US/firefox/addon/livetl/">
-            <v-img src="@/assets/install-firefox.png"/>
+            <v-img src="@/assets/store-images/install-firefox.png"/>
           </a>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col md="8">
+          <v-carousel v-model="showcase">
+            <v-carousel-item v-for="image in images" :key="image" :src="image" />
+          </v-carousel>
         </v-col>
       </v-row>
     </v-container>
@@ -34,6 +42,20 @@
 <script>
 export default {
   name: 'Home',
+  data: () => {
+    return {
+      showcase: 0,
+      // FIXME these don't work like this apparently
+      images: [
+        './assets/showcase-images/cover.png',
+        './assets/showcase-images/holotools-launcher.png',
+        './assets/showcase-images/launcher-buttons.png',
+        './assets/showcase-images/launcher-ui.png',
+        './assets/showcase-images/options.png',
+        './assets/showcase-images/popout.png'
+      ]
+    };
+  },
   computed: {
     getUser() {
       return this.$store.getters.getUser;
