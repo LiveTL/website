c2593eb7d41c253973d1139549cf174774eafb2e
Grumpy Bear <grumpybear4257@gmail.com>
2021-01-06 18:49:54 -0700

    add note about iOS app

---
+++ src/views/IOS.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ios">
+    <v-container>
+      <v-row>
+        <v-col md="6" offset-md="2">
+          <h1>Here's the deal with iOS</h1>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col md="6" offset-md="2">
+          <h3 class="font-weight-regular">
+            The method we used to bring LiveTL to Android, while possible on iOS, is never going to be allowed by Apple
+            to be released on the App Store. <br/>
+            Because of this we would have to write an entire app from scratch, something that no one on the development
+            team currently wants to, or has time to do.
+          </h3>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col md="6" offset-md="2">
+          <h3 class="font-weight-regular">
+            The cost of the Apple Developer Program is also a big factor, as it would cost us $100 USD (per year!) to
+            join. In the future, we may start accepting donations, but for now, we are not able to pay for it.
+          </h3>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col md="6" offset-md="2">
+          <h3 class="font-weight-regular">
+            Because of all this, we aren't really looking at creating a solution for iOS for now. It's not completely
+            out of the question, just out of reach with our current means. If you are a mobile developer (doesn't
+            necessarily have to be iOS specific, but something that supports iOS, such as <a
+            href="https://dotnet.microsoft.com/apps/xamarin">Xamarin</a> or <a href="https://flutter.dev/">Flutter</a>)
+            or you are able to help us out with the Apple Developer Program, please join us on <a
+            href="https://discord.gg/uJrV3tmthg">Discord</a> and let us know! We'd be happy to have
+            you join the team!
+          </h3>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ios'
+};
+</script>
+
+<style scoped>
+
+</style>
