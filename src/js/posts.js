a43a99d20920f6e6900a5ed679d0dc1b5a57c4ad
Kento Nishi <kento24gs@outlook.com>
2021-06-30 10:00:10 -0700

    news post

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,5 +1,5 @@
 export default [{
   title: 'New Android app, translation exports, outsourced TLs, and more!',
-  link: '',
+  link: 'https://www.reddit.com/r/Hololive/comments/ob1gvi/livetl_vtuber_translation_filter_appextension_news/',
   button: 'View post on Reddit'
 }];
